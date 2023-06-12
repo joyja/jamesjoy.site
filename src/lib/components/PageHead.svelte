e661ea23d866fcb3854136478a4365198d93dc25
James Joy <joyja@jarautomation.io>
2023-06-12 03:12:13 +0000

    added meta name=description to pages

--- src/lib/components/PageHead.svelte
+++ src/lib/components/PageHead.svelte
@@ -12,4 +12,5 @@
 	<meta property="og:site_name" content={siteTitle} />
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
+	<meta name="description" content={ description }/>
 </svelte:head>
